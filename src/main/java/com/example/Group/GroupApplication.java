7dd8bfe7fcc747202bfd2f1491f85fbfa7dd47c4
laghrish <hatim.laghrissi@gmail.com>
2024-04-15 16:53:56 +0200

    manipulation projet - début

--- src/main/java/com/example/Group/GroupApplication.java
+++ src/main/java/com/example/Group/GroupApplication.java
@@ -1,13 +1,19 @@
 package com.example.Group;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GroupApplication {
+public class GroupApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(GroupApplication.class, args);
 	}
+
+	@Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello World!");
+    } 
 
 }
