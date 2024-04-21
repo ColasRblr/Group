3560686d40dea3a4277bc88b7e689073d0a4e642
Colas Rabiller <colas.rabiller@gmail>
2024-04-21 19:04:09 +0200

    wip

--- src/test/java/com/example/Group/HomeControllerTest.java
+++ src/test/java/com/example/Group/HomeControllerTest.java
@@ -1,7 +1,10 @@
+package com.example.Group;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
@@ -10,6 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ContextConfiguration(classes = GroupApplication.class)
 public class HomeControllerTest {
 
     @Autowired
