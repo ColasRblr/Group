1fdfa746ef5455b2f078cd28b109d0e8f76b425c
Yradouani <yasmine.radouani@outlook.fr>
2024-04-23 13:26:56 +0200

    création du login

--- src/main/java/com/example/Group/Controller/LoginController.java
+++ src/main/java/com/example/Group/Controller/LoginController.java
@@ -1,12 +1,34 @@
 package com.example.Group.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.ui.Model;
+import com.example.Group.Model.LoginForm;
 
 @Controller
 public class LoginController {
 
-    @GetMapping("/login")
-    public String showLoginPage() {
+    @GetMapping("/")
+    public String showLoginPage(Model model) {
+        model.addAttribute("loginForm", new LoginForm());
         return "login"; 
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam("email") String email,
+                        @RequestParam("password") String password,
+                        RedirectAttributes redirectAttributes) {
+
+    System.out.println("Email: " + email);
+    System.out.println("Password: " + password);
+        
+        return "redirect:/accueil";
+    }
+
+    @GetMapping("/accueil")
+    public String showHomePage() {
+        return "home";
     }
 }
