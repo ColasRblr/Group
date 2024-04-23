82afbf4670b7c3738a63391be2f00c210774889e
Yradouani <yasmine.radouani@outlook.fr>
2024-04-23 13:51:02 +0200

    Ajout de la configuration pour la connection à mysql

---
+++ src/main/java/com/example/Group/Model/User.java
@@ -0,0 +1,33 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "firstname")
+    private String firstname;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "is_admin")
+    private boolean isAdmin;
+
+    @Column(name = "password")
+    private String password;
+
+
+    // Getters et setters
+}
