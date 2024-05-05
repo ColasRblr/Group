e9e2048dcdae43add9147e3840656b0a583c31bd
Colas Rabiller <105796743+ColasRblr@users.noreply.github.com>
2024-05-05 13:18:20 +0200

    Update GroupApplication.java

--- src/main/java/com/example/Group/GroupApplication.java
+++ src/main/java/com/example/Group/GroupApplication.java
@@ -1,25 +1,17 @@
 package com.example.Group;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-
 @EntityScan(basePackages = "com.example.Group.Entity")
 @EnableJpaRepositories(basePackages = "com.example.Group.Repository")
 public class GroupApplication {
 
-
-	public static void main(String[] args) {
-		SpringApplication.run(GroupApplication.class, args);
-	}
-
-	@Override
-    public void run(String... args) throws Exception {
-        System.out.println("Hello World!");
-    } 
+    public static void main(String[] args) {
+        SpringApplication.run(GroupApplication.class, args);
+    }
 
 }
